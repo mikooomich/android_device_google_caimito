983addabc07d45b596451168d9a33d26fb9c89bf
mikooomich <mikooomichael@gmail.com>
2024-12-22 10:32:04 -0500

    Remove ih8sn

--- device-caiman.mk
+++ device-caiman.mk
@@ -501,15 +501,3 @@ PRODUCT_COPY_FILES += \
 
 # Inline Gapps
 include vendor/gapps/arm64/arm64-vendor.mk
-
-# I hate Safety Net
-PRODUCT_PACKAGES += \
-    ih8sn
-
-DEVICE := $(subst lineage_,,$(TARGET_PRODUCT))
-IH8SN_CONFIG_PATH := $(LOCAL_PATH)/prebuilt/etc/ih8sn_$(DEVICE).conf
-
-ifneq ($(wildcard $(IH8SN_CONFIG_PATH)),)
-PRODUCT_COPY_FILES += \
-    $(IH8SN_CONFIG_PATH):$(TARGET_COPY_OUT_VENDOR)/etc/ih8sn.conf
-endif
